4b04e686c3f749a881f130a08fd37e8b2100f1a1
Diana Regina <devdianaregina@gmail.com>
2025-04-01 22:44:09 -0300

    dia 1: instruções de como fazer um copo de café feita

---
+++ desafio-21-dias/dia1.js
@@ -0,0 +1,15 @@
+// Dia 1 - O que é um algoritmo?
+// Irei detalhar instruções para fazer um copo de café
+
+// 1° passo: pegue uma panela pequena;
+//2° passo: pegou a panela? se sim, encha com agua , caso não pegue a panela;
+// 3° passo: acenda o fogo do fogão com fósforo ou com o proprio acendedor do fogão;
+// 4° verifique se o fogo esta asceso;
+// 5° ponha a panela con agua para ferver;
+// 6° esta fervida ? se sim desligue o fogo, caso não espere até ferver;
+// 7° pegue um copo limpo
+// 8° pegue o coador de café;
+// 9° pegue uma colher;
+// 10° com a colher em mãos pegue 3x o pó de café e cada vez vai colocando no coador;
+// 11° so ir despejando a agua no coador com o po aos poucos;
+// 12° ao terminar já esta pronto o seu café
