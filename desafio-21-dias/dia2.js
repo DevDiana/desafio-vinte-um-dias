424160b58d31beb5f4be2fc32632900afcc82c7f
Diana Regina <devdianaregina@gmail.com>
2025-04-02 07:34:14 -0300

    dia 2: sequencia logica

---
+++ desafio-21-dias/dia2.js
@@ -0,0 +1,17 @@
+// Dia 2 - Sequência lógica
+
+// Instruções de como um robô pode escovar os dentes:
+
+// Começe pegando um creme dental
+// Abra a tampa desse creme
+// Pegue a escova de dente com a mão que voce esta livre
+// Espalhe um pouco o creme dental até o meio das cerdas da escova
+// Deixe a escova em cima da pia do banheiro
+// Pegue a tampa do creme dental e feche ele
+// Pegue a escova e escove dente
+// Vai fazendo movimentos circulares em todos os seus dentes com a escova
+// Terminou ? Abra a torneira
+// Lave a escova de dente  e depois de limpa ponha no porta escova
+// Pegue a água com uma mão, leve até a boca para enxaguar e remover a pasta de dente. Repita isso 3 vezes.
+// Feche a torneira
+// Pegue a toalha de rosto e enxugue a sua boca fazendo movimentos circulares
