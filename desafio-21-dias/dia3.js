20c845dd875ab8fc654b1dacfe4a523cfe46512d
Diana Regina <devdianaregina@gmail.com>
2025-04-03 23:16:43 -0300

    desafio do dia 3

---
+++ desafio-21-dias/dia3.js
@@ -0,0 +1,8 @@
+//Exibir no console o nome e a idade do usuário
+
+let nome = "Diana";
+let idade = 29;
+
+let descricaoDaPessoa = `O nome é ${nome} e a idade seria de: ${idade}`;
+
+console.log(descricaoDaPessoa);
